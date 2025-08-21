bfdfdcb544bdd4a2a71d516d97a2f04a1807b79b
Mohammed Yasser <2100921@eng.asu.edu.eg>
2025-08-21 20:25:40 +0300

    Initial project setup

---
+++ university_room_booking/src/test/java/com/sprints/university_room_booking/UniversityRoomBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprints.university_room_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityRoomBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
